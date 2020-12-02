926fa35c3c9ad6d96e1be6b8dc2a520ee9926a73
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2020-12-02 17:12:56 +0000

    Example updates

--- Gaussian Processes/first_gp.py
+++ Gaussian Processes/first_gp.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-def kernel(X1, X2, l=1., sigma_f=1.0):
+def kernel(X1, X2, l=1.0, sigma_f=2.0):
     """
     Isotropic squared exponential kernel.
     
@@ -17,28 +17,32 @@ def kernel(X1, X2, l=1., sigma_f=1.0):
     sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
     return sigma_f**2 * np.exp(-0.5 / l**2 * sqdist)
 
-x  = np.vstack(np.linspace(-12,12, 11))
-y = np.sin(x)
-x_s =  np.vstack(np.linspace(-12., 12., 500))
+z = np.array([])
+for i in range(1,24):
+    z_v = np.random.uniform(-12, 12)
+    z   = np.append(z, z_v)
+    x   = np.vstack(z)
+    y   = 0.1*x*np.sin(x)
+    x_s =  np.vstack(np.linspace(-12., 12., 500))
 
-K = kernel(x, x)
-K_s = kernel(x,x_s)
-K_ss = kernel(x_s, x_s)
-K_i = np.linalg.inv(K)
+    K    = kernel(x, x)
+    K_s  = kernel(x,x_s)
+    K_ss = kernel(x_s, x_s)
+    K_i  = np.linalg.inv(K)
 
-# Equation (7)
-mu_s = K_s.T.dot(K_i).dot(y)
+    # Equation (7)
+    mu_s = K_s.T.dot(K_i).dot(y)
 
-# Equation (8)
-cov_s = K_ss - K_s.T.dot(K_i).dot(K_s)
-N = np.random.multivariate_normal(mu_s.ravel(), cov_s)
+    # Equation (8)
+    cov_s = K_ss - K_s.T.dot(K_i).dot(K_s)
+    N = np.random.multivariate_normal(mu_s.ravel(), cov_s)
 
-x_true = np.linspace(-13,13,500)
-y_true = np.sin(x_true)
-plt.figure()
-plt.plot(x_s, mu_s + cov_s / 2, color = 'blue')
-plt.plot(x_s, mu_s - cov_s / 2, color = 'blue')
-plt.plot(x_s, mu_s, color = 'black')
-plt.plot(x_true, y_true, color = 'red', linestyle = 'dashed')
-plt.scatter(x, y, marker = 'x', color ='red')
-plt.savefig('first_gp.pdf')
+    x_true = np.linspace(-12,12,500)
+    y_true = 0.1*x_true*np.sin(x_true)
+    plt.figure()
+    plt.plot(x_s, mu_s + cov_s / 2, color = 'blue')
+    plt.plot(x_s, mu_s - cov_s / 2, color = 'blue')
+    plt.plot(x_s, mu_s, color = 'black')
+    plt.plot(x_true, y_true, color = 'red', linestyle = 'dashed')
+    plt.scatter(x, y, marker = 'x', color ='red')
+    plt.show()
